610118b08b5b3bb7659031869b3a603d5f23b6c1
felipemv26 <felipemuhalo1@gmail.com>
2024-10-20 16:23:42 -0500

    Actualizar agregar_servicio.php

--- project-root/vistas/administracion/agregar_servicio.php
+++ project-root/vistas/administracion/agregar_servicio.php
@@ -1,6 +1,6 @@
 <?php
 $titulo = "Agregar Servicio";
-include __DIR__ . '/../../vistas/plantillas/header.php';
+include __DIR__ . '/../../vistas/plantillas/admin_header.php';
 ?>
 
 <div class="container mt-4">
